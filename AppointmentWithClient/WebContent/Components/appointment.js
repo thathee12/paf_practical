13a7a65426c370aabfcfc74647ec2f3bd69f4eac
thathee12 <twickramaarachchi@yahoo.com>
2020-05-05 23:06:23 +0530

    Form validations added

--- AppointmentWithClient/WebContent/Components/appointment.js
+++ AppointmentWithClient/WebContent/Components/appointment.js
@@ -1,28 +1,25 @@
-$(document).ready(function()
-{
+$(document).ready(function() {
 	$("#alertSuccess").hide();
 	$("#alertError").hide();
 });
 
-//SAVE ============================================ 
-$(document).on("click", "#btnSave", function(event)
-{  
-	// Clear alerts---------------------  
-	$("#alertSuccess").text("");  
-	$("#alertSuccess").hide();  
-	$("#alertError").text("");  
-	$("#alertError").hide(); 
-	
-	// Form validation-------------------  
-	var status = validateItemForm();  
-	if (status != true)  
-	{   
-		$("#alertError").text(status);   
-		$("#alertError").show();   
-		return;  
-	} 
-	
-	//If valid
+// SAVE 
+$(document).on("click", "#btnSave", function(event) {
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+
+	// Form validation-------------------
+	var status = validateAppointForm();
+	if (status != true) {
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+
+	// If valid
 	var type = ($("#hidAppointIDSave").val() == "") ? "POST" : "PUT";
 
 	$.ajax({
@@ -31,44 +28,46 @@ $(document).on("click", "#btnSave", function(event)
 		data : $("#formAppoint").serialize(),
 		dataType : "text",
 		complete : function(response, status) {
-			onItemSaveComplete(response.responseText, status);
+			onAppointSaveComplete(response.responseText, status);
 		}
 	});
 });
 
-function validateItemForm() {  
-	// CODE  
-	if ($("#placedDate").val().trim() == "")  
-	{   
-		return "Insert placed date"; 
-	} 
-	return true;
-}
+//DELETE
+$(document).on("click", ".btnRemove", function(event) {
+	$.ajax({
+		url : "AppointmentAPI",
+		type : "DELETE",
+		data : "aID=" + $(this).data("appointmentid"),
+		dataType : "text",
+		complete : function(response, status) {
+			onAppointDeleteComplete(response.responseText, status);
+		}
+	});
+});
 
-$(document).on("click", ".btnRemove", function(event) 
-{  $.ajax(  {  
-	url : "AppointmentAPI",   
-	type : "DELETE",   
-	data : "aID=" + $(this).data("appointmentid"),   
-	dataType : "text",   complete : function(response, status)   
-	{    
-		onItemDeleteComplete(response.responseText, status);   
-		}  
-	}); 
-}); 
+$(document)
+		.on(
+				"click",
+				".btnUpdate",
+				function(event) {
+					$("#hidAppointIDSave").val(
+							$(this).closest("tr").find('#hidAppointUpdate')
+									.val());
+					$("#placedDate").val(
+							$(this).closest("tr").find('td:eq(0)').text());
+					$("#appointDate").val(
+							$(this).closest("tr").find('td:eq(1)').text());
+					$("#doctorID").val(
+							$(this).closest("tr").find('td:eq(2)').text());
+					$("#patientID").val(
+							$(this).closest("tr").find('td:eq(3)').text());
+					$("#cause").val(
+							$(this).closest("tr").find('td:eq(4)').text());
 
-$(document).on("click", ".btnUpdate", function(event) {     
-	$("#hidAppointIDSave").val($(this).closest("tr").find('#hidAppointUpdate').val());    
-	$("#placedDate").val($(this).closest("tr").find('td:eq(0)').text());    
-	$("#appointDate").val($(this).closest("tr").find('td:eq(1)').text());    
-	$("#doctorID").val($(this).closest("tr").find('td:eq(2)').text());    
-	$("#patientID").val($(this).closest("tr").find('td:eq(3)').text()); 
-	$("#cause").val($(this).closest("tr").find('td:eq(4)').text());
-	
-}); 
+				});
 
-
-function onItemSaveComplete(response, status) {
+function onAppointSaveComplete(response, status) {
 
 	if (status == "success") {
 		var resultSet = JSON.parse(response);
@@ -95,7 +94,7 @@ function onItemSaveComplete(response, status) {
 	$("#formAppoint")[0].reset();
 }
 
-function onItemDeleteComplete(response, status) {
+function onAppointDeleteComplete(response, status) {
 
 	if (status == "success") {
 		var resultSet = JSON.parse(response);
@@ -120,30 +119,33 @@ function onItemDeleteComplete(response, status) {
 
 }
 
-function onItemUpdateComplete(response, status) {
-
-	if (status == "success") {
-		var resultSet = JSON.parse(response);
-
-		if (resultSet.status.trim() == "success") {
-			$("#alertSuccess").text("Successfully updated.");
-			$("#alertSuccess").show();
-
-			$("#divAppointmentGrid").html(resultSet.data);
-		} else if (resultSet.status.trim() == "error") {
-			$("#alertError").text(resultSet.data);
-			$("#alertError").show();
-		}
-
-	} else if (status == "error") {
-		$("#alertError").text("Error while updating.");
-		$("#alertError").show();
-	} else {
-		$("#alertError").text("Unknown error while updating..");
-		$("#alertError").show();
+// Validate the form
+function validateAppointForm() {
+	// CODE
+	if ($("#placedDate").val().trim() == "") {
+		return "Insert placed date";
+	}
+	if ($("#appointDate").val().trim() == "") {
+		return "Insert placed appointment date";
+	}
+	if ($("#doctorID").val().trim() == "") {
+		return "Insert placed doctor ID";
+	}
+	var tmpDocId = $("#doctorID").val().trim();
+	if (!$.isNumeric(tmpDocId)) {
+		return "Insert a valid numerical value for doctor ID.";
 	}
 
-	$("#hidAppointIDSave").val("");
-	$("#formAppoint")[0].reset();
-}
+	if ($("#patientID").val().trim() == "") {
+		return "Insert placed patient ID";
+	}
+	var tmpPid = $("#patientID").val().trim();
+	if (!$.isNumeric(tmpPid)) {
+		return "Insert a valid numerical value for patient ID.";
+	}
+	if ($("#cause").val().trim() == "") {
+		return "Insert the cause of appointment";
+	}
 
+	return true;
+}
